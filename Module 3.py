e07f166f45b3a38ad4ec3ab62999463751354dd9
mustah21 <Mustafa.Ahmad@metropolia.fi>
2024-08-29 15:31:19 +0500

    Module 3

---
+++ Module 3.py
@@ -0,0 +1,11 @@
+zander_length = float(input("Please enter your zander length in centimeters: ") )
+size_limit >= 42
+
+If zander_length == size_limit:
+   print ("Great Catch")
+elif
+length_needed = 42 - zander_length:
+    print(f"Please release the fish back into the sea because it is smaller by" & length_needed & "Cm" )
+
+
+
