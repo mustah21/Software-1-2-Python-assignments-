25752cad902c5beea7e552a0eaddcc1eeaabe96a
mustah21 <Mustafa.Ahmad@metropolia.fi>
2024-08-29 18:32:26 +0500

    Module 3

--- Module 3.py
+++ Module 3.py
@@ -1,11 +1,63 @@
-zander_length = float(input("Please enter your zander length in centimeters: ") )
-size_limit >= 42
+import math
 
-If zander_length == size_limit:
+#exercise 1
+"""
+zander_length = int(input("Please enter your zander length in centimeters: ") )
+
+if zander_length > 41:
    print ("Great Catch")
-elif
-length_needed = 42 - zander_length:
-    print(f"Please release the fish back into the sea because it is smaller by" & length_needed & "Cm" )
+elif zander_length < 41:
+    length_needed = 42 - zander_length
+    print(f"Please release the fish back into the sea because it is smaller by {length_needed} cm")
+
+#exercise 2
+
+cabin_class = str(input("Please enter your cabin class in capital letters: "))
+
+if cabin_class == "LUX":
+    print ("Upper deck with a balcony")
+elif cabin_class == "A":
+    print ("Above the car deck, equipped with a window")
+elif cabin_class == "B":
+    print ("Windowless cabin with car deck")
+elif cabin_class == "C":
+    print ("Windowless cabin below the car deck")
+else: print ("invalid class")
+
+
+#exercise 3
+
+bio_gender = str(input("Please enter your gender: "))
+hg_value = int(input("Please enter your hemoglobin value: "))
+
+
+if bio_gender == "male"or bio_gender == "male":
+    if hg_value > 155:
+        print("Too high hg value for adult male")
+    if hg_value < 117:
+        print ("Too low hemoglobin value for adult male")
+    else:
+        print("Normal Hg for adult male")
+
+if bio_gender == "female" or bio_gender == "Female":
+    if hg_value < 134:
+        print("Too low hemoglobin value for adult female")
+    if hg_value > 167:
+        print("Too high hg value for adult female")
+    else:
+        print("Normal Hg for adult female")
+        
+
+#exercise 4
+
+year = int(input("Please enter your year: "))
+
+if year % 4 == 0:
+             print ("it is a leap year" )
+elif year % 100 == 0 and year % 400 == 0:
+             print ("it is a leap year")
+else: print ("Its not a leap year")
 
 
 
+"""
---
+++ Module 4.py
@@ -0,0 +1,8 @@
+#exercise 1
+
+number = 1
+
+while number > 1000:
+    if number % 3 == 0:
+        print(number)
+        number += 1
