049e1624e7714ac9843b61d0db09e8fcd21ac4be
mustah21 <mustafa.ahmad@metropolia.fi>
2024-09-27 00:38:25 +0500

    Module 8

--- Module 8.py
+++ Module 8.py
@@ -1,45 +1,51 @@
 import mysql.connector
-"""
+
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
-         database='airport',
+         database='flight_game',
          user='root',
-         password='thebest99',
+         password='asus',
          autocommit=True,
 )
-
+"""
+#Write a program that asks the user to enter the ICAO code of an airport.
+#The program fetches and prints out the corresponding airport name
+#and location (town) from the airport database used on this course.
+#The ICAO codes are stored in the ident column of the airport table.
 
 def fetch_airports_by_icao(icao):
-    sql = "SELECT, id, ident, type, name FROM airport"
-    sql += "where ident = '" +icao+ "'"
+    sql = f"SELECT id, ident, type, name FROM airport WHERE ident = '{icao}'"
+
     db_cursor = connection.cursor()
     db_cursor.execute(sql)
     query_result = db_cursor.fetchall()
+
     if db_cursor.rowcount > 0:
-            for row in query_result:
-                print(f"Airport name{row[3]}, and its type is {row[2]}")
+        for row in query_result:
+            print(f"Airport name: {row[3]}, and its type is: {row[2]}")
     else:
-        print("airport not found")
-    return
+        print("No airport found with the given ICAO code.")
+
 
 icao = input("Give me the ICAO code of the airport: ")
 fetch_airports_by_icao(icao)
-
 """
-#exercise 2
+#Exercise 2
 
-def fetch_airports_by_country(name):
-    sql = "SELECT type, COUNT(*) FROM airport WHERE ident ='" +name+ "'" + GROUP BY type"
-    print(sql)
-    db_cursor = db_connection.cursor()
+def fetch_by_areacode(name):
+    sql = f"SELECT COUNT(*), type ,name FROM airport WHERE iso_country='{name}' GROUP BY type ORDER BY COUNT(*) DESC"
+    db_cursor = connection.cursor()
     db_cursor.execute(sql)
     query_result = db_cursor.fetchall()
+
     if db_cursor.rowcount > 0:
-            for row in query_result:
-                print(f"Airport type{row[0]}, there are now{row[1]} of them ")
+        for row in query_result:
+            print(f"Airport name: {row[2]}, its type is: {row[1]}")
     else:
-        print("airport not found")
-    return
-name = input("Give me the name of the country: ")
-fetch_airports_by_country(name)
+        print("No airport found for the given area code.")
+
+name = input("Please enter name of the country: ")
+fetch_by_areacode(name)
+
+#Exercise 3
