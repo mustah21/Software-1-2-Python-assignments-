26f784da0509d81d2bbea732579747c40dc2a4b8
mustah21 <mustafa.ahmad@metropolia.fi>
2024-10-28 00:54:03 +0200

    o

--- module 10.py
+++ module 10.py
@@ -63,30 +63,6 @@ new.run_elevator(0,6)
 new.run_elevator(3,4)
 new.fire_alarm()
 
-
-
-#Write a main program that creates an 8000-kilometer race called Grand Demolition Derby.
-#The new race is given a list of ten cars similarly to the earlier exercise.
-#The main program simulates the progressing of the race by calling the hour_passes in a loop,
-#after which it uses the race_finished method to check if the race has finished.
-#The current status is printed out using the print_status method every ten hours and then once more at the end of the race.
-
-#This exercise continues the previous car race exercise from the last exercise set.
-#Write a Race class that has the following properties:
-#name, distance in kilometers and a list of cars participating in the race.
-#The class has an initializer that receives the name, kilometers,
-# and car list as parameters and sets their values to the corresponding properties in the class.
-
-# The class has the following methods:
-
-#hour_passes, which performs the operations done once per hour in the original exercise:
-#generates a random change of speed for each car and calls their drive method.
-
-#print_status, which prints out the current information of each car as a clear, formatted table.
-
-#race_finished, which returns True if any of the cars has reached the finish line,
-#meaning that they have driven the entire distance of the race.
-
 #Task 4
 class Race:
     def __init__(self, name, distance_km, cars):
@@ -99,7 +75,6 @@ class Race:
             speed_change  = random.randint(-10,10)
             car.accelerate(speed_change)
             car.drive(1)
-
 
     def print_status(self):
         for car in self.cars:
@@ -118,7 +93,9 @@ while not race.race_finished():
     hours_passed += 1
 
     if hours_passed % 10 == 0:
+        print(f"\n After {hours_passed} hours")
         race.print_status()
+
 
 race.print_status()
 print(f"Race finished after {hours_passed} hours!")
